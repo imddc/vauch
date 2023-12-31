2cf2e12e61ce11896966ac3692890f0e8e0459cb
Du Cheng <easyhoo2426@163.com>
2024-01-01 02:20:46 +0800

    fix: toggle

--- src/composables/index.ts
+++ src/composables/index.ts
@@ -1,10 +1,10 @@
 export const isDark = useDark()
 export const toggleDark = (e: MouseEvent) => {
+  const toggle = useToggle(isDark)
   document.documentElement.style.setProperty('--x', e.clientX + 'px')
   document.documentElement.style.setProperty('--y', e.clientY + 'px')
 
   useViewTransition(() => {
-    document.documentElement.classList.toggle('dark')
-    useToggle(isDark)()
+    toggle()
   })
 }
